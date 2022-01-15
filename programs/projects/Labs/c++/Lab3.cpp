2ecee7621ed3051dedc6a2ff0e47b29229a2070a
danhof1 <ddoyon1@pride.hofstra.edu>
2022-01-15 18:15:13 -0500

    a

--- programs/projects/Labs/c++/Lab3.cpp
+++ programs/projects/Labs/c++/Lab3.cpp
@@ -13,7 +13,6 @@ class Sieve{
         
     }
     void initialize(int n,bool arr[]){
-        arr[n+1];
         for(int x=0;x<sizeof(arr)/sizeof(arr[0]);x++){
             arr[x]=true;
         }
@@ -25,13 +24,13 @@ class Sieve{
          }
          else
          {
-            for (int p=2; p*p<=size; p++)
+            for (int p = 2; p*p <= size; p++)
             {
                 // If prime[p] is not changed, then it is a prime
                 if (arr[p] == true)
                     {
                         // Update all multiples of p
-                        for (int i=p*2; i<=size; i += p)
+                        for (int i = p*2; i <= size; i += p)
                         arr[i] = false;
                     }
             }
@@ -43,7 +42,7 @@ class Sieve{
     //2) create this function to print the list
     //for debugging purposes.
     void printList(){
-        for(int i=0; i < size; i++){
+        for(int i = 0; i < size; i++){
             cout<< list[i] << ",";
         }
         cout << list[size-1]<< endl << endl;
@@ -53,7 +52,7 @@ class Sieve{
 
     //print the prime numbers 
     void printPrimes(bool arr[]){
-       for (int p=2; p<=sizeof(arr)/sizeof(arr[0]); p++){
+       for (int p = 2; p <= (sizeof(arr) /sizeof(arr[0])); p++){
             if (arr[p])
                 cout << p << " ";
        }
@@ -67,15 +66,15 @@ class Sieve{
         
     }
 
-}
+};
 
 int main(){
     Sieve sieve;
     int listSize = 0;
     cout<<"How long is your Sieve list?"<<endl;
     cin>>listSize;
-    bool arr[listSize];
-    sieve.initialize(listSize,arr[listSize]);
-    sieve.runAlgorithm(listSize,arr[listSize]);
-    sieve.printPrimes(arr[listSize]);
+    bool* arr= new bool[listSize];
+    sieve.initialize(listSize,arr);
+    sieve.runAlgorithm(listSize,arr);
+    sieve.printPrimes(arr);
 };
